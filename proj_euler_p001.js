a7a6fa1359f4900b28c74eacc079ea8cbcaa2ed5
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-29 12:55:30 -0500

    solved p#001 with js

--- proj_euler_p001.js
+++ proj_euler_p001.js
@@ -1,4 +1,10 @@
-console.log("Hello, JavaScript!");
-var number = 42;
-document.getElementById("numberDisplay").textContent = number;
-console.log(number)
+var sum = 0
+
+for (var t = 0; t <= 999; t++) {
+  if (t % 3 === 0) {
+    sum += t
+  } else if (t % 5 === 0) {
+    sum += t
+  }
+}
+console.log("sum is:", sum)
