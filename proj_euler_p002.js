2e052849f8fd5737c2afb7e5109eb9362af469a1
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-06-29 13:40:14 -0500

    trying to recreate p#002 in js

---
+++ proj_euler_p002.js
@@ -0,0 +1,16 @@
+// let username = prompt("Please enter your name:");
+// console.log("+ ", username)
+
+var sum = 0
+var a = 1
+var b = 2
+var storage = 0
+
+for (b < 4000000) {
+  storage = b
+  b += a
+  a = storage
+  if (b % 2 === 0) {
+
+  }
+}
