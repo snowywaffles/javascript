150324b840060489e5d4a1ac02e47ad7dc5c790c
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-05 13:11:16 -0500

    solved the problem. I still don't know what 'const' does though, so I should probably find out lol

--- proj_euler_p002.js
+++ proj_euler_p002.js
@@ -7,15 +7,20 @@ const rl = readline.createInterface({
 
 rl.question('Please enter the desired number to generate Fibonacci numbers up to: ', (input) => {
   console.log('+', input);
-  var sum = parseInt(input);
-  var a = 1;
-  var b = 2;
-  var storage = 0;
+  var max_number = parseInt(input);
+  var sum = 0;
+  var a = 0;
+  var b = 1;
 
-  if (b % 2 === 0) {
-    console.log('b is even');
+  for (; b <= max_number;) {
+    b += a
+    a = b - a
+    console.log('a:', a, '| b:', b)
+    if (b % 2 === 0) {
+      console.log('b is even');
+      sum += b
+    }
   }
-  console.log(sum, a, b, storage)
-
+  console.log(sum)
   rl.close();
 });
